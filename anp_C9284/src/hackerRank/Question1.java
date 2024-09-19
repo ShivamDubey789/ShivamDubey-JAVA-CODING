2accc3b88d3d39e8848ba57b6e046721974fcd03
ShivamDubey789 <shivamdubeysdh@gmail.com>
2024-09-19 08:00:03 +0530

    SEPT19

---
+++ anp_C9284/src/hackerRank/Question1.java
@@ -0,0 +1,12 @@
+package hackerRank;
+
+public class Question1 {
+
+
+	    public static void main(String[] args) {
+	        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+	        
+	        System.out.println("Hello, World.");
+	        System.out.println("Hello, Java.");
+	    }
+}
